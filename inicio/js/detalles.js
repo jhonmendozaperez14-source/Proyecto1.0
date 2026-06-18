9948189c88c491ea09970302967111516deb1a90
Jhon <jhonmendozaperez14@gmail.com>
2026-06-17 20:08:47 -0500

    cambios :)

--- inicio/js/detalles.js
+++ inicio/js/detalles.js
@@ -4,7 +4,7 @@
 const baseDeDatos = {
     emma: {
         titulo: "Emma Elite",
-        precio: "S/ 1,500.00",
+        precio: "S/ 2,599.00",
         imagen: "../img/colchon emma elite.webp",
         desc: "Híbrido de alta gama con tecnología AirGrid para una mejor distribución del peso y confort supremo. Se adapta ergonómicamente a cualquier tipo de cuerpo disminuyendo la transferencia de movimiento en la cama.",
         caract: ["Firmeza: Dinámica adaptable", "Material: Células abiertas AirGrid", "Garantía: 10 años de fábrica", "Altura total: 30 centímetros"]
@@ -23,6 +23,20 @@ const baseDeDatos = {
         desc: "El espacio perfecto para compartir un descanso reparador en pareja. Minimiza por completo la transferencia de ondas de movimiento gracias a su amortiguación central balanceada.",
         caract: ["Firmeza: Balanceada Media", "Material: Espuma indeformable de alta resiliencia", "Garantía: 5 años certificados", "Altura total: 28 centímetros"]
     },
+    paraiso: {
+    titulo: "Paraíso Su Majestad",
+    precio: "S/ 1,899.00",
+    imagen: "../img/colchon-paraiso-mta.webp",
+    desc: "Colchón premium con sistema de resortes Pocket que brindan total independencia de movimiento. Su acolchado de espuma Zebra de alta densidad garantiza un soporte ergonómico ideal para cuidar la postura.",
+    caract: ["Firmeza: Intermedia - Firme", "Material: Resortes Pocket y Espuma Zebra", "Garantía: 12 años de fábrica", "Altura total: 32 centímetros"]
+},
+rosen: {
+    titulo: "Rosen Ergo T",
+    precio: "S/ 1,459.00",
+    imagen: "../img/colchon-rose-ergo.webp",
+    desc: "Excelente equilibrio entre confort y soporte gracias a su parrilla de resortes Bonnell templados. Cuenta con una cubierta de tela tejido de punto acolchada que es súper suave al tacto y mejora la ventilación.",
+    caract: ["Firmeza: Intermedia", "Material: Resortes Bonnell y Tela Tejido de Punto", "Garantía: 10 años de fábrica", "Altura total: 28 centímetros"]
+},
     viscoelastica: {
         titulo: "Espuma Viscoelástica",
         precio: "S/ 950.00",
@@ -39,7 +53,7 @@ const baseDeDatos = {
     }
 };
 
-// 2. Capturar el parámetro 'id' desde la URL
+
 const parametrosURL = new URLSearchParams(window.location.search);
 const idProducto = parametrosURL.get('id');
 
